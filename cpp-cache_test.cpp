f7c9cca7db402516c25aa9a0d14656dea07692bb
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2024-11-08 09:58:17 -0500

    feat: add basic cache implementation
    This commit adds the basic (but incomplete) implementation of a cache using a hash table with separate chaining for collision resolution. The cache is designed to handle key-value pairs and employs a least recently used (LRU) policy for replacement when the cache is full. The implementation also includes features for locking and unlocking entries, as well as expanding the cache capacity.

---
+++ cpp-cache_test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lrleon on 11/8/24.
+//
+
+# include <iostream>
+# include <gtest/gtest.h>
+# include "cpp-cache.H"
+
+using namespace std;
+using namespace testing;
+
+TEST(CacheTest, Test1)
+{
+
+}
