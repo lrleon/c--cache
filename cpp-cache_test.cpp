c9119c24f5a0c3d6d0e427fb28eeefe04ec14b00
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2024-11-12 17:00:08 -0500

    Fixing wrapper to user provided hash function og Key type

--- cpp-cache_test.cpp
+++ cpp-cache_test.cpp
@@ -10,27 +10,55 @@
 using namespace std;
 using namespace testing;
 
+size_t hash_fct(const int &key)
+{
+  return key;
+}
+
+struct SimpleFixture : public Test
+{
+  static bool miss_handler(const int &key, int *data, void *user_data)
+  {
+    return true;
+  }
+
+  Cache<int, int> cache;
+
+  SimpleFixture()
+  : cache(10, miss_handler, hash_fct)
+  {
+    // empty
+  }
+
+  ~SimpleFixture()
+  {
+    // empty
+  }
+
+
+};
+
 TEST(cache_entry, basic)
 {
   using Cache = Cache<int, int>;
   Cache::CacheEntry cache_entry;
-  
+
   ASSERT_EQ(cache_entry.status(),
             Cache::CacheEntry::Status::AVAILABLE);
   ASSERT_TRUE(cache_entry.link_lru()->is_empty());
   ASSERT_EQ(cache_entry.get_ad_hoc_code(), 0);
-  
+
   cache_entry.set_data(10);
-  
+
   ASSERT_EQ(cache_entry.get_data(), 10);
-  
+
   cache_entry.set_status(Cache::CacheEntry::Status::READY);
-  
+
   ASSERT_EQ(cache_entry.status(),
             Cache::CacheEntry::Status::READY);
-  
+
   cache_entry.set_ad_hoc_code(1);
-  
+
   ASSERT_EQ(cache_entry.get_ad_hoc_code(), 1);
 
   cache_entry.set_positive_ttl_exp_time(high_resolution_clock::now() + 1s);
@@ -48,27 +76,27 @@ TEST(cache_entry, basic)
   sleep(1);
 
   ASSERT_TRUE(cache_entry.negative_ttl_expired(high_resolution_clock::now()));
-  
+
   cout << "CacheEntry: " << cache_entry.get_data() << endl;
 }
 
 TEST(cache_entry, key_copy_works)
 {
   Cache<vector<int>, int>::CacheEntry cache_entry;
-  
+
   vector<int> key = {1, 2, 3};
   cache_entry.set_key(key);
-  
+
   ASSERT_EQ(cache_entry.key(), key);
 }
 
 TEST(cache_entry, key_move_works)
 {
   Cache<vector<int>, int>::CacheEntry cache_entry;
-  
+
   vector<int> key = {1, 2, 3};
   cache_entry.set_key(move(key));
-  
+
   ASSERT_EQ(cache_entry.key().size(), 3);
   ASSERT_EQ(cache_entry.key(), vector<int>({1, 2, 3}));
   ASSERT_EQ(key.size(), 0);
@@ -78,24 +106,24 @@ TEST(cache_entry, key_move_works)
 TEST(cache_entry, data_copy_works)
 {
   Cache<int, vector<int>>::CacheEntry cache_entry;
-  
+
   vector<int> data = {1, 2, 3};
   cache_entry.set_data(data);
-  
+
   ASSERT_EQ(cache_entry.data(), data);
 }
 
 TEST(cache_entry, data_move_works)
 {
   Cache<int, vector<int>>::CacheEntry cache_entry;
-  
+
   vector<int> data = {1, 2, 3};
   cache_entry.set_data(move(data));
-  
+
   ASSERT_EQ(cache_entry.data().size(), 3);
   ASSERT_EQ(cache_entry.data(), vector<int>({1, 2, 3}));
   ASSERT_EQ(data.size(), 0);
   ASSERT_TRUE(data.empty());
-  
+
   OLhashTable<int, vector<int>> hash_table(10);
 }
