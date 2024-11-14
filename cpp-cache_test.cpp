1fc8b2e676657ff543013436f80523bd8f28d5a1
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2024-11-14 10:13:08 -0500

    test: add more test cases for Cache class
    Add unit tests for the Cache class, including basic insert, LRU functionality, touch operation, and retrieval with has and touch.

--- cpp-cache_test.cpp
+++ cpp-cache_test.cpp
@@ -10,84 +10,6 @@
 using namespace std;
 using namespace testing;
 
-size_t hash_fct(const int &key)
-{
-  return key;
-}
-
-struct SimpleFixture : public Test
-{
-  static bool miss_handler(const int &key, int *data, void *user_data)
-  {
-    return true;
-  }
-
-  Cache<int, int> cache;
-
-  SimpleFixture()
-    : cache(10,  1s, 1s, miss_handler)
-  {
-    // empty
-  }
-};
-
-TEST_F(SimpleFixture, basic)
-{
-  ASSERT_EQ(cache.capacity(), 10);
-  ASSERT_EQ(cache.size(), 0);
-  //cache is empty
-  ASSERT_FALSE(cache.has(1));
-  //insert a key-value pair
-  cache.insert(1, 10);
-  //cache is not empty
-  ASSERT_EQ(cache.size(), 1);
-  // check lru entry
-  ASSERT_EQ(cache.get_lru_entry()->key(), 1);
-  //key is in cache
-  ASSERT_TRUE(cache.has(1));
-  // wait ttl to expire
-  sleep(1);
-  // key is not in cache
-  ASSERT_FALSE(cache.has(1));
-  // check expired key was removed from cache
-  ASSERT_EQ(cache.size(), 0);
-}
-
-TEST_F(SimpleFixture, lru) {
-  cache.insert(1, 10);
-  cache.insert(2, 20);
-
-  ASSERT_EQ(cache.size(), 2);
-
-  // check lru entry
-  ASSERT_EQ(cache.get_lru_entry()->key(), 1);
-  ASSERT_NE(cache.get_lru_entry()->key(), 90);
-
-  // remove lru entry
-  cache.remove_entry_from_hash_table(cache.get_lru_entry());
-
-  ASSERT_EQ(cache.size(), 1);
-
-  // check lru entry
-  ASSERT_EQ(cache.get_lru_entry()->key(), 2);
-}
-
-TEST_F(SimpleFixture, touch) {
-  cache.insert(1, 10);
-  cache.insert(2, 20);
-
-  ASSERT_EQ(cache.size(), 2);
-
-  // touch key 1
-  ASSERT_TRUE(cache.touch(1));
-  // check lru entry
-  ASSERT_EQ(cache.get_lru_entry()->key(), 2);
-  ASSERT_NE(cache.get_lru_entry()->key(), 1);
-  // touch unknown key
-  ASSERT_FALSE(cache.touch(90));
-
-}
-
 TEST(cache_entry, basic)
 {
   using Cache = Cache<int, int>;
@@ -168,16 +90,138 @@ TEST(cache_entry, data_move_works)
   Cache<int, vector<int>>::CacheEntry cache_entry;
 
   vector<int> data = {1, 2, 3};
-  cache_entry.set_data(move(data));
+  cache_entry.set_data(std::move(data));
 
   ASSERT_EQ(cache_entry.data().size(), 3);
   ASSERT_EQ(cache_entry.data(), vector<int>({1, 2, 3}));
   ASSERT_EQ(data.size(), 0);
   ASSERT_TRUE(data.empty());
-
-  OLhashTable<int, vector<int>> hash_table(10);
 }
 
+struct SimpleFixture : public Test
+{
+  static bool miss_handler(const int &key, int *data, void *user_data)
+  {
+    return true;
+  }
 
+  Cache<int, int> cache;
 
+  SimpleFixture()
+    : cache(5, 1s, 1s, miss_handler)
+  {
+    // empty
+  }
+};
+
+TEST_F(SimpleFixture, basic)
+{
+  ASSERT_EQ(cache.capacity(), 10);
+  ASSERT_EQ(cache.size(), 0);
+  //cache is empty
+  ASSERT_FALSE(cache.has(1));
+  //insert a key-value pair
+  cache.insert(1, 10);
+  //cache is not empty
+  ASSERT_EQ(cache.size(), 1);
+  // check lru entry
+  ASSERT_EQ(cache.get_lru_entry()->key(), 1);
+  //key is in cache
+  ASSERT_TRUE(cache.has(1));
+  // wait ttl to expire
+  sleep(1);
+  // key is not in cache
+  ASSERT_FALSE(cache.has(1));
+  // check expired key was removed from cache
+  ASSERT_EQ(cache.size(), 0);
+}
+
+TEST_F(SimpleFixture, lru)
+{
+  cache.insert(1, 10);
+  cache.insert(2, 20);
+
+  ASSERT_EQ(cache.size(), 2);
+
+  // check lru entry
+  ASSERT_EQ(cache.get_lru_entry()->key(), 1);
+  ASSERT_NE(cache.get_lru_entry()->key(), 90);
+
+  // remove lru entry
+  cache.remove_entry_from_hash_table(cache.get_lru_entry());
+
+  ASSERT_EQ(cache.size(), 1);
+
+  // check lru entry
+  ASSERT_EQ(cache.get_lru_entry()->key(), 2);
+}
+
+TEST_F(SimpleFixture, insert_with_has_and_touch)
+{
+  ASSERT_TRUE(cache.insert(1, 10))
+            << "It should be able to insert a key-value pair";
+
+  ASSERT_TRUE(cache.has(1))
+            << "It should be able to find the key";
+
+  {
+    auto p_lru = cache.get_lru();
+    ASSERT_EQ(p_lru.first, 1);
+    ASSERT_EQ(p_lru.second, 10);
+
+    auto p_mru = cache.get_mru();
+    ASSERT_EQ(p_mru.first, 1);
+    ASSERT_EQ(p_mru.second, 10);
+
+    // test positive key expiration
+    sleep(1);
+    ASSERT_FALSE(cache.has(1))
+              << "It should not be able to find the key, since it has expired";
+  }
+
+  ASSERT_TRUE(cache.insert(1, 10))
+            << "It should be able to insert a key-value pair";
+  ASSERT_TRUE(cache.insert(2, 20))
+            << "It should be able to insert a key-value pair";
+  ASSERT_TRUE(cache.insert(3, 30))
+            << "It should be able to insert a key-value pair";
+
+  {
+    auto p_lru = cache.get_lru();
+    ASSERT_EQ(p_lru.first, 1);
+    ASSERT_EQ(p_lru.second, 10);
+
+    auto p_mru = cache.get_mru();
+    ASSERT_EQ(p_mru.first, 3);
+    ASSERT_EQ(p_mru.second, 30);
+  }
+
+  ASSERT_TRUE(cache.touch(2))
+            << "It should be able to touch the key";
+
+  auto p_mru = cache.get_mru();
+  ASSERT_EQ(p_mru.first, 2);
+  ASSERT_EQ(p_mru.second, 20);
+
+  auto p_lru = cache.get_lru();
+  ASSERT_EQ(p_lru.first, 1);
+  ASSERT_EQ(p_lru.second, 10);
+}
+
+TEST_F(SimpleFixture, touch)
+{
+  cache.insert(1, 10);
+  cache.insert(2, 20);
+
+  ASSERT_EQ(cache.size(), 2);
+
+  // touch key 1
+  ASSERT_TRUE(cache.touch(1));
+  // check lru entry
+  ASSERT_EQ(cache.get_lru_entry()->key(), 2);
+  ASSERT_NE(cache.get_lru_entry()->key(), 1);
+  // touch unknown key
+  ASSERT_FALSE(cache.touch(90));
+
+}
 
